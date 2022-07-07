5dfaa9d63323f7c4d05fe77d6bf3969c063dbbb6
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-07-08 00:02:22 +0300

    Main component added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import "./App.css";
 import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header";
 import { Search } from "./components/common/Search";
+import { Pagination } from "./components/pagination/Pagination";
+import { UsersList } from "./components/users-list/UsersList";
 
 function App() {
   return (
     <div>
       <Header />
 
-      <main class="main">
-        <section class="card users-container">
+      <main className="main">
+        <section className="card users-container">
 
           <Search />
+
+          <UsersList />
+
+          <Pagination />
 
         </section>
       </main>
--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -25,7 +25,7 @@ export const Search = () => {
 
             <div className="filter">
                 <span>Search Criteria:</span>
-                <select name="criteria" className="criteria" value="">
+                <select name="criteria" className="criteria" defaultValue={""}>
                     <option value="">Not selected</option>
                     <option value="">First Name</option>
                     <option value="">Last Name</option>
